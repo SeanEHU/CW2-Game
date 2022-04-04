9eb7ec2de7cc6347c15bc7e0b27eeb552a489450
SeanEHU <101595096+SeanEHU@users.noreply.github.com>
2022-04-04 11:50:27 +0100

    Game class expanded
    Added attributes and methods to the game class

--- game/src/Game.java
+++ game/src/Game.java
@@ -1,5 +1,18 @@
 public class Game {
     
-    
+    public Location[] locationArray = null;
+    public Player player = null;
+    private boolean gameWon = false;
+
+    public Game() {
+
+        this.locationArray = loadLocations();
+        this.player = new Player;
+
+    }
+
+    private Location[] loadLocations() {
+
+    }
 
 }
