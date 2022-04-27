13432219415430d122551ca3a297cab01000a92e
SomeGuyEatingPie <seandavidjohnson132@gmail.com>
2022-04-27 15:47:41 +0100

    Now Using CSV for map file

---
+++ game/src/DTEMM.java
@@ -0,0 +1,8 @@
+public class DTEMM {
+
+    public static void main(String[] args) {
+        
+        Game newGame = new Game();
+
+    }
+}
--- game/src/Game.java
+++ game/src/Game.java
@@ -1,31 +1,50 @@
 import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 public class Game {
     
-    public Location[4][4] locationArray = null;
-    public Player player = null;
+    public Location[][] locationArray = new Location[4][4];
+    //public Player player = null;
     private boolean gameWon = false;
 
     public Game() {
 
-        this.locationArray = loadLocations();
-        this.player = new Player;
+        this.loadLocations();
+        //this.player = new Player;
+
+        for(int y = 0; y <= 3; y++){
+            for(int x = 0 ; x <= 3; x++){
+                
+                System.out.println(this.locationArray[x][y].getName());
+                System.out.println(this.locationArray[x][y].getDesc());
+                
+            }
+        }
 
     }
 
-    private Location[] loadLocations() {
+    private void loadLocations() {
         
-        FileReader mapReader = new FileReader('.src/map.json');
-        BufferedReader bufferedReader = new BufferedReader(mapReader);
+        try{
+            FileReader mapReader = new FileReader("./src/map.csv");
+            BufferedReader bufferedReader = new BufferedReader(mapReader);
+            String line = "";
 
-        for(y = 0, i <= 3, i++){
-            for( = 0, i <= 3, i++){
-                line = bufferedReader.readLine();
-                obj = new JSONParser().parse(line);
-                json.add(obj);
-                Location[x][y] = new Location(obj.get("name"), obj.get("desc"))
+            for(int y = 0; y <= 3; y++){
+                for(int x = 0 ; x <= 3; x++){
+                    line = bufferedReader.readLine();
+                    System.out.println(line);
+                    String[] values = line.split(",");
+                    this.locationArray[x][y] = new Location(values[0], values[1]);
+
+                    bufferedReader.close();
+                }
             }
         }
+        catch(IOException e){
+        }
+        finally{}
 
 
     }
